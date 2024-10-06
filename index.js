79e1cef30aa1ee688a04d018976c9a192b41643e
tilakuppar <tilliveer22@gmail.com>
2024-10-07 01:59:11 +0530

    Describe the changes you made

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mysql = require('mysql');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
